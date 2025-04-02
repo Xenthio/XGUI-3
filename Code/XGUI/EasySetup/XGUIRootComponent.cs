295bde810099c3c15f672fa4c235664be1e68eef
Xenthio <thegooster88@gmail.com>
2025-04-03 08:25:08 +1100

    Update XGUIRootComponent.cs

--- Code/XGUI/EasySetup/XGUIRootComponent.cs
+++ Code/XGUI/EasySetup/XGUIRootComponent.cs
@@ -1,11 +1,19 @@
 ﻿using Sandbox;
 namespace XGUI;
+[Title( "XGUI Root Component" )]
 public class XGUIRootComponent : PanelComponent
 {
 	public XGUIRootPanel XGUIPanel { get; private set; }
 	protected override void OnStart()
 	{
+		// check if there's a screenpanel here, create one if not.
+		if ( GameObject.Components.TryGet<ScreenPanel>( out var screenPanel ) == false )
+		{
+			var pnl = GameObject.AddComponent<ScreenPanel>();
+			pnl.AutoScreenScale = false;
+		}
 		base.OnStart();
+
 		XGUIPanel = new XGUIRootPanel();
 		Panel.AddChild( XGUIPanel );
 
